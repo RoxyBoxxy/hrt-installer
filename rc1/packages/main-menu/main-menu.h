c7bf71056f956acba665a2d4c348f3b1498d02ed
Joey Hess <joeyh@debian.org>
2004-08-07 15:14:41 +0000

    tag rc1
    (this tag has newer versions for a few packages that have been changed
    since final rc1 uploads)

---
+++ rc1/packages/main-menu/main-menu.h
@@ -0,0 +1,29 @@
+#ifdef DODEBUG
+#include <assert.h>
+#define ASSERT(x) assert(x)
+#define DPRINTF(fmt,args...) fprintf(stderr, fmt, ##args)
+#else
+#define ASSERT(x) /* nothing */
+#define DPRINTF(fmt,args...) /* nothing */
+#endif
+
+#define BUFSIZE		4096
+
+#define MAIN_MENU	"debian-installer/main-menu"
+#define MISSING_PROVIDE "debian-installer/missing-provide"
+#define ITEM_FAILURE	"debian-installer/main-menu/item-failure"
+
+#include <debian-installer.h>
+
+#define NEVERDEFAULT 900
+
+enum
+{
+	EXIT_OK			= 0,
+	EXIT_BACKUP		= 10,
+	EXIT_QUIT		= 11,
+	EXIT_RESTART		= 12,
+};
+
+/* vim: noexpandtab sw=8
+ */
