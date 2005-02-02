5ce7938dca31336aaed354ed5ccc12bc5698776f
Joey Hess <joeyh@debian.org>
2005-02-02 22:12:41 +0000

    tagging version 1.14

---
+++ ddetect/1.14/archdetect.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cdebconf/debconfclient.h>
+
+#include "archdetect.h"
+
+int main(int argc, char *argv[])
+{
+        const char *subarch;
+
+	if (!(subarch = subarch_analyze()))
+ 		return EXIT_FAILURE;
+
+	if (strstr(argv[0], "postinst") || (argc > 1 && strcmp(argv[1], "postinst") == 0)) {
+                struct debconfclient *debconf;
+		debconf = debconfclient_new();
+		debconf_set(debconf, "debian-installer/kernel/subarchitecture", subarch);
+	} else
+		printf("%s/%s\n", CPU_TEXT, subarch);
+
+	return 0;
+}
