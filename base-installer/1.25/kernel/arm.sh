85081cf80b728d69635aeaa57c1b49a13cb89bed
Joey Hess <joeyh@debian.org>
2005-08-17 17:10:36 +0000

    tagging version 1.25

---
+++ base-installer/1.25/kernel/arm.sh
@@ -0,0 +1,31 @@
+arch_get_kernel_flavour () {
+	case "$SUBARCH" in
+		netwinder|bast|riscpc|riscstation|lart)
+			echo "$SUBARCH"
+			return 0
+		;;
+		ads*)
+			# Currently no kernel in debian for ADS boards.
+			echo "none"
+			return 0
+		;;
+		*)
+			warning "Unknown $ARCH subarchitecture '$SUBARCH'."
+			return 1
+		;;
+	esac
+}
+
+arch_check_usable_kernel () {
+	# Subarchitecture must match exactly.
+	if expr "$1" : ".*-$2\$" >/dev/null; then return 0; fi
+	return 1
+}
+
+arch_get_kernel () {
+	if [ "$1" = none ]; then
+		echo none
+	else
+		echo "kernel-image-$KERNEL_VERSION-$1"
+	fi
+}
