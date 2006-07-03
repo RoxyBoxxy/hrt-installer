0063a5dbf1616d866d3b38727b2aa47b89dd0f51
Martin Michlmayr <tbm@cyrius.com>
2006-07-03 15:44:48 +0000

    tag 1.62

---
+++ base-installer/1.62/kernel/hppa.sh
@@ -0,0 +1,25 @@
+arch_get_kernel_flavour () {
+	echo "$MACHINE"
+	return 0
+}
+
+arch_check_usable_kernel () {
+	if expr "$1" : '.*-parisc-' >/dev/null; then return 0; fi
+	if expr "$1" : '.*-parisc$' >/dev/null; then return 0; fi
+	if expr "$1" : '.*-32.*' >/dev/null; then return 0; fi
+	if [ "$2" = parisc ]; then return 1; fi
+	if expr "$1" : '.*-parisc64.*' >/dev/null; then return 0; fi
+	if expr "$1" : '.*-64.*' >/dev/null; then return 0; fi
+
+	# default to usable in case of strangeness
+	warning "Unknown kernel usability: $1 / $2"
+	return 0
+}
+
+arch_get_kernel () {
+	CPUS="$(grep ^processor "$CPUINFO" | tail -n 1 | cut -d: -f2)"
+	if [ -z "$CPUS" ] || [ "$CPUS" -ne 0 ]; then
+		echo "linux-image-$1-smp"
+	fi
+	echo "linux-image-$1"
+}
