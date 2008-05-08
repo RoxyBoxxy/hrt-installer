ba404a17f266d1fd37ffd8574d4d226c3afec5e2
Otavio Salvador <otavio@debian.org>
2008-05-08 03:05:30 +0000

    tagging version 1.91

---
+++ base-installer/1.91/kernel/hppa.sh
@@ -0,0 +1,21 @@
+arch_get_kernel_flavour () {
+	echo "$MACHINE"
+	return 0
+}
+
+arch_check_usable_kernel () {
+	if echo "$1" | grep -Eq -- "-parisc(32)?(-.*)?$"; then return 0; fi
+	if [ "$2" = parisc ]; then return 1; fi
+	if echo "$1" | grep -Eq -- "-parisc64?(-.*)?$"; then return 0; fi
+
+	# default to usable in case of strangeness
+	warning "Unknown kernel usability: $1 / $2"
+	return 0
+}
+
+arch_get_kernel () {
+	if [ "$NUMCPUS" ] && [ "$NUMCPUS" -gt 1 ]; then
+		echo "linux-image-$KERNEL_MAJOR-$1-smp"
+	fi
+	echo "linux-image-$KERNEL_MAJOR-$1"
+}
