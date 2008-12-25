3bfb8f5d0cf3dd81ea6ad044b624de6f382c4c4b
Frans Pop <elendil@planet.nl>
2008-12-25 20:26:15 +0000

    Update lenny branch for updates targeted at Lenny

--- lenny/packages/rootskel/src/sbin/steal-ctty.c
+++ lenny/packages/rootskel/src/sbin/steal-ctty.c
@@ -1,8 +1,18 @@
+/*
+ * Licensed under GPLv2
+ *
+ * Adapted for Debian Installer by Jérémy Bobbio <lunar.debian.org> from
+ * cttyhack from busybox 1.11, which is
+ *
+ * Copyright (c) 2007 Denys Vlasenko <vda.linux@googlemail.com>
+ */
+
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char ** argv)
 {
