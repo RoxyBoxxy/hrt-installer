3510b071f2fe9042d359a2a58025dc4f6f6ae47b
Frans Pop <elendil@planet.nl>
2009-11-28 23:19:31 +0000

    Ignore preseeded suite when installing from CD with base system
    Backported from trunk.
    
    If we're installing from a CD that contains base system packages we
    want to always use the release corresponding to that.

--- etch/packages/choose-mirror/choose-mirror.c
+++ etch/packages/choose-mirror/choose-mirror.c
@@ -24,6 +24,9 @@ static char *protocol = NULL;
 static char *country  = NULL;
 int show_progress = 1;
 
+/* Are we installing from a CD that includes base system packages? */
+static int base_on_cd = 0;
+
 /*
  * Returns a string on the form "DEBCONF_BASE/protocol/supplied". The
  * calling function is responsible for freeing the string afterwards.
@@ -126,7 +129,7 @@ static char *get_default_suite(void) {
 
 	/* Check for a preseeded suite/codename. */
 	debconf_get(debconf, DEBCONF_BASE "suite");
-	if (strlen(debconf->value) > 0)
+	if (! base_on_cd && strlen(debconf->value) > 0)
 		suite = strdup(debconf->value);
 	else
 		suite = strdup(PREFERRED_DISTRIBUTION);
@@ -213,7 +216,6 @@ int find_suite (void) {
 	return ret;
 }
 
-static int base_on_cd = 0;
 static int check_base_on_cd(void) {
 	FILE *fp;
 	if ((fp = fopen("/cdrom/.disk/base_installable", "r"))) {
